9619195f8524207cf5ff5fadade89c580efdffa4
rashkabarre <waddanisoamli@gmail.com>
2026-09-25 00:36:01 +0300

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const PRODUCTS = [
     description: "Kiyow waa aar yar oo soo maray marxalado kala duwan, ilaa ugu dambayntii uu noqday boqor dhexdiisa walaalihiis.",
     price: "$1",
     thumbnail: "aarkii-kiyow.jpg",
-    preview: "",
+    preview: "https://player.mediadelivery.net/play/761815/2861540b-30a0-4331-9e66-d267627aac29",
     paymentUrl: "#"
   }
 ];
