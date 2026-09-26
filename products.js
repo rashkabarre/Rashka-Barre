e8eea288c4e34649bbd589d62e85befcd1375288
rashkabarre <waddanisoamli@gmail.com>
2026-09-26 10:21:54 +0300

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const PRODUCTS = [
     id: "aarkii-kiyow",
     title: "Aarkii Kiyow",
     description: "Kiyow waa aar yar oo soo maray marxalado kala duwan, ilaa ugu dambayntii uu noqday boqor dhexdiisa walaalihiis.",
-    price: "$1",
+    price: "$0.25",
     thumbnail: "aarkii-kiyow.jpg",
     preview: "",
     paymentUrl: "https://sifalo.co/gbbt"
